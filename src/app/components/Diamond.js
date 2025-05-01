b475c4348fdf17d87d94675f96ce2ce944a32dac
Arthur <menacejuco1163@yahoo.com>
2025-05-01 15:54:38 -0700

    added diamond component part 2

--- src/app/components/Diamond.js
+++ src/app/components/Diamond.js
@@ -1,11 +1,29 @@
-
+import styles from "../Header.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Diamond() {
-    return (
-        <>
-        <div>
-
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className={styles.dottedline__right}>
+        <Image
+          src="/Rectangle_2778.png"
+          alt="taketestbutton"
+          width={300}
+          height={600}
+        />
+      </div>
+      <div className={styles.diamond__button}>
+        <Link href="/pagetwo">
+          TAKE TEST
+          <Image
+            src="/buttin-icon-shrunk1.png"
+            alt="taketestbutton"
+            width={50}
+            height={50}
+          />
+        </Link>
+      </div>
+    </>
+  );
 }
--- src/app/components/TakeTest.js
+++ src/app/components/TakeTest.js
@@ -8,16 +8,16 @@ export default function TakeTest() {
   return (
     <>
       <div className={styles.rectangles__dotted2}>
-        <Diamond width={350} height={350} dotted borderColorClass />
+        {/* <Diamond width={350} height={350} dotted borderColorClass /> */}
       </div>
       <div className={styles.taketest__button}>
         <Link href="/pagetwo">
-          <Image
+          {/* <Image
             src="/button-icon-text-shrunk.png"
             alt="taketestbutton"
             width={125}
             height={45}
-          />
+          /> */}
         </Link>
       </div>
     </>
